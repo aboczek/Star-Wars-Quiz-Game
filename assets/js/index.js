078f9636137fc3c9c8540720d0c4abe4a1a367c0
aboczek <adam.boczekwork@gmail.com>
2022-10-15 11:59:20 +0000

    starting code in js questions

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// Questions
+let questionContainer = [
+    {
+        question: "What is the name of Yoda's home?",
+        answer: {
+            a: Naboo,
+            b: Kashyyk,
+            c: Dagobah
+        },
+        correct: "c"
+    }
+
+]
