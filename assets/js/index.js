b886db3473c5688aa4bc17191716791f085079a0
aboczek <adam.boczekwork@gmail.com>
2022-10-15 19:54:47 +0000

    debugging start game arrow function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,112 +1,90 @@
 // Questions and answers
 let questionContainer = [{
         question: "What is the name of Yoda's home?",
-        answer: {
-            a: "Naboo",
-            b: "Kashyyk",
-            c: "Dagobah"
-        },
-        correct: "c"
+        answer1: "Naboo",
+        answer2: "Kashyyk",
+        answer3: "Dagobah",
+        correct: "3"
     },
 
     {
         question: "C-3P0 is fluent in how many languages?",
-        answer: {
-            a: "3",
-            b: "2 milion",
-            c: "Over 60 million languages"
-        },
-        correct: "c"
+        answer1: "3",
+        answer2: "2 milion",
+        answer3: "Over 60 million languages",
+        correct: "3"
     },
 
     {
         question: "Who built C-3P0?",
-        answer: {
-            a: "Anakin Skywalker",
-            b: "Boba Fett",
-            c: "Princes Leia"
-        },
-        correct: "a"
+        answer1: "Anakin Skywalker",
+        answer2: "Boba Fett",
+        answer3: "Princes Leia",
+        correct: "1"
     },
 
     {
         question: "How old is Yoda when he dies?",
-        answer: {
-            a: "69",
-            b: "900",
-            c: "232"
-        },
-        correct: "b"
+        answer1: "69",
+        answer2: "900",
+        answer3: "232",
+        correct: "2"
     },
 
     {
         question: "Who is the young Jedi Knight who becomes Darth Vader?",
-        answer: {
-            a: "Luke Skywalker",
-            b: "Anakin Skywalker",
-            c: "Obi Wan Kenobi"
-        },
-        correct: "b"
+        answer1: "Luke Skywalker",
+        answer2: "Anakin Skywalker",
+        answer3: "Obi Wan Kenobi",
+        correct: "2"
     },
 
     {
         question: "What is the ‘Rule of 2’?",
-        answer: {
-            a: "There can only be 2 Sith Lords at one time.",
-            b: "Eye for an eye",
-            c: "Brunch and dinner"
-        },
-        correct: "a"
+        answer1: "There can only be 2 Sith Lords at one time.",
+        answer2: "Eye for an eye",
+        answer3: "Brunch and dinner",
+        correct: "1"
     },
 
     {
         question: "Who stars as the famous Han Solo in the original Star Wars films?",
-        answer: {
-            a: "Brad Pitt",
-            b: "Harrison Ford",
-            c: "Daniel Radcliffe"
-        },
-        correct: "b"
+        answer1: "Brad Pitt",
+        answer2: "Harrison Ford",
+        answer3: "Daniel Radcliffe",
+        correct: "1"
     },
 
     {
         question: "Who is Luke and Leia’s mother?",
-        answer: {
-            a: "Padmé Amidala",
-            b: "Ahsoka Tano",
-            c: "Jyn Erso"
-        },
-        correct: "a"
+        answer1: "Padmé Amidala",
+        answer2: "Ahsoka Tano",
+        answer3: "Jyn Erso",
+        correct: "1"
     },
 
     {
         question: " What age did Padmé Amidala become a queen?",
-        answer: {
-            a: "9",
-            b: "14",
-            c: "18"
-        },
-        correct: "b"
+        answer1: "9",
+        answer2: "14",
+        answer3: "18",
+        correct: "2"
     },
 
     {
         question: "Who tried to save Han Solo by disguising and infiltrating Jabba’s palace?",
-        answer: {
-            a: "Master Yoda",
-            b: "Princess Leia",
-            c: "Chewbacca"
-        },
-        correct: "b"
+        answer1: "Master Yoda",
+        answer2: "Princess Leia",
+        answer3: "Chewbacca",
+        correct: "2"
     },
 
     {
-        question:  "How many engines does the X-Wing Fighter have?",
-        answer: {
-            a: "2",
-            b: "3",
-            c: "4"
-        },
-        correct: "c"
+        question: "How many engines does the X-Wing Fighter have?",
+        answer1: "2",
+        answer2: "3",
+        answer3: "4",
+        correct: "3"
     }
 ]
 // Declaring all variables to get elements from html file
@@ -115,7 +93,8 @@ const playGame = document.getElementById("play-game");
 const highscore = document.getElementById("score-on");
 const rules = document.getElementById("rules");
 const gameWindow = document.getElementById("game-window");
-const questions = document.getElementById("question");
+const question = document.getElementById("question");
+const answers = Array.from(document.getElementsByClassName("answers"));
 const gameResult = document.getElementById("game-result");
 const gameScore = document.getElementById("game-score");
 const mainMenu = document.getElementById("main-menu");
@@ -125,29 +104,46 @@ window.addEventListener('DOMContentLoaded', () => {
     rules.style.display = "none";
     gameWindow.style.display = "none";
     highscore.style.display = "none";
-    gameResult.style.display = "none";    
+    gameResult.style.display = "none";
     playGame.addEventListener("click", startGame);
     highscore.addEventListener("click", showScore);
 })
 
-// Starting the rules before game
-function startGameRules () {
-    rules.style.display = "block";
-    gameBoard.style.display = "none";
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
-    setTimeout (function() {
-        rules.style.display = "none"
-        gameWindow.style.display = "flex";
-    }, 5000);
-}
-
+const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 10;
 
-// Start the game
+// // //Starting the rules before game
+
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questionContainer];
+    availableQuestions = [... questionContainer];
+    console.log(availableQuestions);
+    getNewQuestion ();
+
+        rules.style.display = "flex";
+    gameBoard.style.display = "none";
+
+    setTimeout(function () {
+        rules.style.display = "none"
+        gameWindow.style.display = "flex";
+    }, 5000);
+};
+
+getNewQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    answers.forEach(answer => {
+        const number = answer.dataset["number"];
+        answer.innterText = currentQuestion["answer" * number];
+    })
 }
-
-
