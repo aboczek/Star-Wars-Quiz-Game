28fe35f7225a8675e6a6d9fff3053678712f4887
aboczek <adam.boczekwork@gmail.com>
2022-10-20 21:19:32 +0000

    fixing spelling bug in json

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,84 +1,5 @@
 // Questions and answers
-let questionContainer = [{
-        question: "What is the name of Yoda's home?",
-        answer1: "Naboo",
-        answer2: "Kashyyk",
-        answer3: "Dagobah",
-        correct: 3
-    },
 
-    {
-        question: "C-3P0 is fluent in how many languages?",
-        answer1: "3",
-        answer2: "2 milion",
-        answer3: "Over 60 million languages",
-        correct: 3
-    },
-
-    {
-        question: "Who built C-3P0?",
-        answer1: "Anakin Skywalker",
-        answer2: "Boba Fett",
-        answer3: "Princes Leia",
-        correct: 1
-    },
-
-    {
-        question: "How old is Yoda when he dies?",
-        answer1: "69",
-        answer2: "900",
-        answer3: "232",
-        correct: 2
-    },
-
-    {
-        question: "Who is the young Jedi Knight who becomes Darth Vader?",
-        answer1: "Luke Skywalker",
-        answer2: "Anakin Skywalker",
-        answer3: "Obi Wan Kenobi",
-        correct: 2
-    },
-
-    {
-        question: "What is the ‘Rule of 2’?",
-        answer1: "There can only be 2 Sith Lords at one time.",
-        answer2: "Eye for an eye",
-        answer3: "Brunch and dinner",
-        correct: 1
-    },
-
-    {
-        question: "Who stars as the famous Han Solo in the original Star Wars films?",
-        answer1: "Brad Pitt",
-        answer2: "Harrison Ford",
-        answer3: "Daniel Radcliffe",
-        correct: 2
-    },
-
-    {
-        question: "Who is Luke and Leia’s mother?",
-        answer1: "Padmé Amidala",
-        answer2: "Ahsoka Tano",
-        answer3: "Jyn Erso",
-        correct: 1
-    },
-
-    {
-        question: " What age did Padmé Amidala become a queen?",
-        answer1: "9",
-        answer2: "14",
-        answer3: "18",
-        correct: 2
-    },
-
-    {
-        question: "Who tried to save Han Solo by disguising and infiltrating Jabba’s palace?",
-        answer1: "Master Yoda",
-        answer2: "Princess Leia",
-        answer3: "Chewbacca",
-        correct: 2
-    },
-];
 
 const gameBoard = document.querySelector("#game-board");
 const playGame = document.querySelector("#play-game");
@@ -108,23 +29,16 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 let currentQuestion = {};
+let questionContainer = []
 
 
-
-// Hidding unecessary content from viewing and loading Dom
-window.addEventListener('DOMContentLoaded', () => {
-    rules.style.display = "none";
-    gameWindow.style.display = "none";
-    leadersBoard.style.display = "none";
-    gameResult.style.display = "none";
-    // listenning to clicks on play, highscore, return to main menu
-    playGame.addEventListener("click", startGame);
-    highScoreBtn.addEventListener("click", showScore);
-    returnToMenu.addEventListener("click", returnToMainMenu);
-    mainMenu.addEventListener("click", returnToMainMenuTwo);
-});
-
-
+fetch("assets/json/questions.json")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadQuestions => {
+        questionContainer = loadQuestions
+    });
 
 
 const startGame = () => {
@@ -247,6 +161,19 @@ const returnToMainMenuTwo = () => {
 
 
 scoreList.innerHTML = highScore.map(score => {
-        return `<li class="high-score">${score.name} - ${score.score}</li>`;
-    })
-    .join("");
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+}).join("");
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    rules.style.display = "none";
+    gameWindow.style.display = "none";
+    leadersBoard.style.display = "none";
+    gameResult.style.display = "none";
+    // listenning to clicks on play, highscore, return to main menu
+    playGame.addEventListener("click", startGame);
+    highScoreBtn.addEventListener("click", showScore);
+    returnToMenu.addEventListener("click", returnToMainMenu);
+    mainMenu.addEventListener("click", returnToMainMenuTwo);
+});
